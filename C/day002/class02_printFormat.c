2993dea1f88b0413df44eef64edcb081d20b9d94
Min Jeong <96562720+baby-beepboop@users.noreply.github.com>
2025-11-07 09:25:31 +0900

    Create class02_printFormat.c

---
+++ C/day002/class02_printFormat.c
@@ -0,0 +1,11 @@
+// n진수 출력
+#include <stdio.h>
+
+int main() {
+	printf("=== Print n-digit ===\n");
+	printf("Print in decimal: %d\n", 10);		    // %d: 10진수로 출력
+	printf("Print in octal:   %o\n", 10);		    // %o: 8진수로 출력
+	printf("Print in hex:     %0x\n\n", 10);    // %0x: 16진수로 출력
+
+  return 0;
+}
