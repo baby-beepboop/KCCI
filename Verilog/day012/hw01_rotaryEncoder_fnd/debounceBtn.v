a4fe7d94df135cef3f4791a31c119800c2922b2a
Min Jeong <96562720+baby-beepboop@users.noreply.github.com>
2025-11-10 08:54:15 +0900

    Create debounceBtn.v

---
+++ Verilog/day012/hw01_rotaryEncoder_fnd/debounceBtn.v
@@ -0,0 +1,34 @@
+module debounceBtn(
+    input clk, rst,
+    input tick,
+    
+    input      btnRaw,
+    output reg btnDb
+    );
+    
+    localparam CNT_MAX = 10;    // 안정에 필요한 연속 tick 수 (10ms)
+
+    reg [3:0] cnt;
+    always @(posedge clk, posedge rst) begin
+        if (rst) begin
+            cnt <= 0;
+            btnDb <= 0;
+        end
+
+        else if (tick) begin
+            if (btnRaw == btnDb) begin
+                cnt <= 0;
+            end
+
+            else begin
+                cnt <= cnt + 1'b1;
+
+                if (cnt >= CNT_MAX-1) begin    // 10ms가 지나면 debouncing
+                    btnDb <= btnRaw;
+                    cnt <= 0;
+                end
+            end
+        end
+    end
+
+endmodule
